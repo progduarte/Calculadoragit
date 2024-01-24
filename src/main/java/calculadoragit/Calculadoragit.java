a0b3ca1612cc2abe874c9d40aefafc0d504028eb
progduarte <progvasconcelos12@gmail.com>
2024-01-24 15:12:19 -0300

    variaveis de classe adicionadas,int x, int y, int soma, subtracao, multiplicacao, divisao

--- src/main/java/calculadoragit/Calculadoragit.java
+++ src/main/java/calculadoragit/Calculadoragit.java
@@ -5,7 +5,18 @@ package calculadoragit;
  * @author dev
  */
 
+        
+
 public class Calculadoragit {
+    
+    /** 
+         * declaracao de variaveis de classe
+         */
+        static int x, y;
+        static int soma;
+        static int subtracao;
+        static int multiplicacao;
+        static int divisao;
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
