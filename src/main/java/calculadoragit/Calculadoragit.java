bceb91f85f451ccfba9cabe0a2d355047ca7a823
progduarte <progvasconcelos12@gmail.com>
2024-01-24 14:49:26 -0300

    Commit inicial

---
+++ src/main/java/calculadoragit/Calculadoragit.java
@@ -0,0 +1,13 @@
+package calculadoragit;
+
+/**
+ *
+ * @author dev
+ */
+
+public class Calculadoragit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
